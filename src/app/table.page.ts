a7d5bc9eafb9b6f88ff7ae9e86ec45a5d2f1e6c3
James Johanson <james16johanson@gmail.com>
2019-11-20 14:36:25 -0700

    Added the table as a new page. Not working correctly right now.

---
+++ src/app/table.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CensusService } from './census.service';
+
+@Component({
+	selector: 'app-table',
+	templateUrl: 'table.page.html',
+	styleUrls: ['table.page.scss'],
+})
+export class TablePage {
+	public searchResults: any[];
+	public myCharacters: any[] = [];
+	public achievements: any[];
+
+	constructor(private census: CensusService) { }
+
+	public async test() {
+		const results = await this.census.getCharacterWithAchievements(
+			this.myCharacters.map(c => c.id)
+		);
+		console.log(results);
+		this.achievements = results.character_list;
+	}
+}
