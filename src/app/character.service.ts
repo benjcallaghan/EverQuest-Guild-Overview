2747ce08776bcdf3245312574fa964d9720384ef
Ben Callaghan <benj.callaghan@gmail.com>
2025-10-17 22:48:13 -0600

    rebuilt add character on the behavior subject

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
-import { defer, ReplaySubject, share } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { CensusCharacter } from './census.service';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class CharacterService {
-  characters$ = defer(() => this.getAllCharacters()).pipe(
-    share({
-      connector: () => new ReplaySubject(1),
-      resetOnRefCountZero: false
-    })
-  );
+    #characters = new BehaviorSubject<CensusCharacter[]>([]);
+    characters$ = this.#characters.asObservable();
 
-  constructor(private readonly storage: Storage) {}
+    constructor(private readonly storage: Storage) {
+        this.getAllCharacters().then((characters) =>
+            this.#characters.next(characters)
+        );
+    }
 
-  public async getAllCharacters(): Promise<CensusCharacter[]> {
-    return (await this.storage.get('characters')) ?? [];
-  }
+    public async getAllCharacters(): Promise<CensusCharacter[]> {
+        return (await this.storage.get('characters')) ?? [];
+    }
 
-  public async saveCharacters(characters: CensusCharacter[]): Promise<void> {
-    await this.storage.set('characters', characters);
-  }
+    public async saveCharacters(characters: CensusCharacter[]): Promise<void> {
+        await this.storage.set('characters', characters);
+    }
+
+    public async add(character: CensusCharacter): Promise<void> {
+        const characters = [...this.#characters.value, character];
+        characters.sort((a, b) => a.displayname.localeCompare(b.displayname));
+        this.#characters.next(characters);
+        await this.saveCharacters(characters);
+    }
 }
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { asapScheduler, defer, Observable } from 'rxjs';
@@ -11,50 +11,44 @@ import { CharacterService } from '../character.service';
     selector: 'app-settings',
     imports: [IonicModule, FormsModule, NgIf, NgFor, AsyncPipe],
     templateUrl: './settings.page.html',
-    styleUrls: ['./settings.page.scss']
+    styleUrls: ['./settings.page.scss'],
 })
 export default class SettingsPage {
-  searching = false;
-  searchName: string;
-  searchServer: number;
-  searchResults$: Observable<CensusCharacter[]>;
-  selected: Observable<CensusCharacter[]> = this.characterService.characters$;
+    searching = false;
+    searchName: string;
+    searchServer: number;
+    searchResults$: Observable<CensusCharacter[]>;
+    selected: Observable<CensusCharacter[]> = this.characterService.characters$;
 
-  constructor(
-    private readonly census: CensusService,
-    private readonly characterService: CharacterService
-  ) {}
+    constructor(
+        private readonly census: CensusService,
+        private readonly characterService: CharacterService
+    ) {}
 
-  async searchForCharacter(): Promise<void> {
-    this.searchResults$ = defer(() => {
-      this.searching = true;
-      return this.census.getCharactersByName(
-        this.searchName,
-        this.searchServer
-      );
-    }).pipe(
-      subscribeOn(asapScheduler),
-      map((data) => data.character_list),
-      tap({
-        next: () => (this.searching = false),
-        error: () => (this.searching = false),
-      })
-    );
-  }
+    async searchForCharacter(): Promise<void> {
+        this.searchResults$ = defer(() => {
+            this.searching = true;
+            return this.census.getCharactersByName(
+                this.searchName,
+                this.searchServer
+            );
+        }).pipe(
+            subscribeOn(asapScheduler),
+            map((data) => data.character_list),
+            tap({
+                next: () => (this.searching = false),
+                error: () => (this.searching = false),
+            })
+        );
+    }
 
-  async add(character: CensusCharacter): Promise<void> {
-    // if (this.selected.every((c) => c.id !== character.id)) {
-    //   this.selected.push(character);
-    //   this.selected.sort((a, b) => a.displayname.localeCompare(b.displayname));
-    // }
+    async add(character: CensusCharacter): Promise<void> {
+        await this.characterService.add(character);
+    }
 
-    // await this.characterService.saveCharacters(this.selected);
-  }
-
-  async remove(index: number): Promise<void> {
-    // this.selected.splice(index, 1);
-    // this.selected.sort((a, b) => a.displayname.localeCompare(b.displayname));
-
-    // await this.characterService.saveCharacters(this.selected);
-  }
+    async remove(index: number): Promise<void> {
+        // this.selected.splice(index, 1);
+        // this.selected.sort((a, b) => a.displayname.localeCompare(b.displayname));
+        // await this.characterService.saveCharacters(this.selected);
+    }
 }
