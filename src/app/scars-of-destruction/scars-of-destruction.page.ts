2a14128affe94fb6a75aee953d2c222d458e64a7
Ben Callaghan <benj.callaghan@gmail.com>
2025-10-18 00:07:50 -0600

    added Weapon for the Ages

--- src/app/scars-of-destruction/scars-of-destruction.page.ts
+++ src/app/scars-of-destruction/scars-of-destruction.page.ts
@@ -19,7 +19,7 @@ export default class ScarsOfDestructionPage {
 
     characters = this.characterService.characters$;
 
-    thawedRivalry(character: CensusCharacter): string {
+    thawedRivalry(character: CensusCharacter): string | string[] {
         const part4 = '1346401727';
         const part3 = '3458523164';
         const part2 = '3106132106';
@@ -57,7 +57,7 @@ export default class ScarsOfDestructionPage {
         return 'Not Started';
     }
 
-    bondsOfMischief(character: CensusCharacter): string {
+    bondsOfMischief(character: CensusCharacter): string | string[] {
         const part4 = '224232166';
         const part3 = '2469992261';
         const part2 = '3829278675';
@@ -95,7 +95,7 @@ export default class ScarsOfDestructionPage {
         return 'Not Started';
     }
 
-    depthsOfDestruction(character: CensusCharacter): string {
+    depthsOfDestruction(character: CensusCharacter): string | string[] {
         const part2 = '926301496';
         const part1 = '2923392130';
 
@@ -104,7 +104,7 @@ export default class ScarsOfDestructionPage {
         }
 
         if (part2 in character.misc.completed_quest_list) {
-            return 'Start Part 3';
+            return 'Complete';
         }
         if (part2 in character.misc.quest_list) {
             return this.getProgressText(character, part2);
@@ -119,9 +119,58 @@ export default class ScarsOfDestructionPage {
         return 'Not Started';
     }
 
-    private getProgressText(character: CensusCharacter, quest: string): string {
+    weaponForTheAges(character: CensusCharacter): string | string[] {
+        const platinum = '1925862104';
+        const gold = '97346126';
+        const silver = '2611607533';
+        const bronze = '3970877307';
+        const copper = '1973917377';
+
+        if (character.misc === undefined) {
+            return 'Refresh';
+        }
+
+        if (platinum in character.misc.completed_quest_list) {
+            return 'Complete';
+        }
+        if (platinum in character.misc.quest_list) {
+            return this.getProgressText(character, platinum);
+        }
+        if (gold in character.misc.completed_quest_list) {
+            return 'Start Platinum';
+        }
+        if (gold in character.misc.quest_list) {
+            return this.getProgressText(character, gold);
+        }
+        if (silver in character.misc.completed_quest_list) {
+            return 'Start Gold';
+        }
+        if (silver in character.misc.quest_list) {
+            return this.getProgressText(character, silver);
+        }
+        if (bronze in character.misc.completed_quest_list) {
+            return 'Start Silver';
+        }
+        if (bronze in character.misc.quest_list) {
+            return this.getProgressText(character, bronze);
+        }
+        if (copper in character.misc.completed_quest_list) {
+            return 'Start Bronze';
+        }
+        if (copper in character.misc.quest_list) {
+            return this.getProgressText(character, copper);
+        }
+
+        return 'Not Started';
+    }
+
+    private getProgressText(
+        character: CensusCharacter,
+        quest: string
+    ): string[] {
         console.assert(character.misc !== undefined);
-        return character.misc!.quest_list[quest].requiredItem_list[0]
-            .progress_text;
+        return character.misc!.quest_list[quest].requiredItem_list.map(
+            (i) => i.progress_text
+        );
     }
 }
