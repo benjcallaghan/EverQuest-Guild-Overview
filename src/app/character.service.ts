8bd31d00c1b0b491a0f4a79ba262a580b19833ea
Ben Callaghan <benj.callaghan@gmail.com>
2025-10-25 17:57:17 -0600

    replaced imperative state updates with reactive actions

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -1,16 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import {
     BehaviorSubject,
-    exhaustMap,
+    from,
     map,
-    OperatorFunction,
-    pipe,
-    Subject,
-    Subscription,
+    mergeScan,
+    Observable,
+    of,
+    share,
     tap,
-    withLatestFrom,
 } from 'rxjs';
 import { CensusCharacter } from './census.service';
 import { build } from './daybreak-census-options';
@@ -19,42 +18,90 @@ interface CharacterSearchResults {
     character_list: CensusCharacter[];
 }
 
+interface Command {
+    execute(
+        characters: CensusCharacter[],
+        service: CharacterService
+    ): Observable<CensusCharacter[]>;
+}
+
+class Initialize implements Command {
+    execute(
+        characters: CensusCharacter[],
+        service: CharacterService
+    ): Observable<CensusCharacter[]> {
+        return from(service.getSavedCharacters());
+    }
+}
+
+class Refresh implements Command {
+    execute(
+        characters: CensusCharacter[],
+        service: CharacterService
+    ): Observable<CensusCharacter[]> {
+        return service.getOnlineCharacters(characters);
+    }
+}
+
+class Add implements Command {
+    constructor(private character: CensusCharacter) {}
+
+    execute(
+        characters: CensusCharacter[],
+        service: CharacterService
+    ): Observable<CensusCharacter[]> {
+        return of([...characters, this.character]);
+    }
+}
+
+class Remove implements Command {
+    constructor(private character: CensusCharacter) {}
+
+    execute(
+        characters: CensusCharacter[],
+        service: CharacterService
+    ): Observable<CensusCharacter[]> {
+        const index = characters.findIndex((c) => c.id === this.character.id);
+        return of(index > -1 ? characters.toSpliced(index, 1) : characters);
+    }
+}
+
 @Injectable({
     providedIn: 'root',
 })
-export class CharacterService implements OnDestroy {
-    #refresh = new Subject<void>();
-    #refreshSubscription: Subscription;
+export class CharacterService {
     #refreshing = new BehaviorSubject<boolean>(false);
     refreshing$ = this.#refreshing.asObservable();
 
-    #characters = new BehaviorSubject<CensusCharacter[]>([]);
-    characters$ = this.#characters.asObservable();
+    #actions = new BehaviorSubject<Command>(new Initialize());
+    characters$ = this.#actions.pipe(
+        // TODO: Change to exhaustScan (an operator that doesn't exist)
+        mergeScan(
+            (characters, action) => action.execute(characters, this),
+            [] as CensusCharacter[]
+        ),
+        tap((characters) =>
+            characters.sort((a, b) =>
+                a.displayname.localeCompare(b.displayname)
+            )
+        ),
+        tap((characters) => this.saveCharacters(characters)),
+        share({
+            connector: () => new BehaviorSubject([] as CensusCharacter[]),
+            resetOnRefCountZero: false,
+        })
+    );
 
     constructor(
         private readonly storage: Storage,
         private readonly http: HttpClient
-    ) {
-        this.getAllCharacters().then((characters) =>
-            this.#characters.next(characters)
-        );
-        this.#refreshSubscription = this.#refresh
-            .pipe(
-                tap(() => this.#refreshing.next(true)),
-                this.getCharactersOnline(),
-                tap(() => this.#refreshing.next(false))
-            )
-            .subscribe(async (refreshed) => {
-                this.#characters.next(refreshed);
-                await this.saveCharacters(refreshed);
-            });
+    ) {}
+
+    public getAllCharacters(): Promise<CensusCharacter[]> {
+        return this.getSavedCharacters();
     }
 
-    ngOnDestroy(): void {
-        this.#refreshSubscription.unsubscribe();
-    }
-
-    public async getAllCharacters(): Promise<CensusCharacter[]> {
+    public async getSavedCharacters(): Promise<CensusCharacter[]> {
         return (await this.storage.get('characters')) ?? [];
     }
 
@@ -62,79 +109,62 @@ export class CharacterService implements OnDestroy {
         await this.storage.set('characters', characters);
     }
 
-    public async add(character: CensusCharacter): Promise<void> {
-        const characters = [...this.#characters.value, character];
-        characters.sort((a, b) => a.displayname.localeCompare(b.displayname));
-        this.#characters.next(characters);
-        await this.saveCharacters(characters);
+    public add(character: CensusCharacter): void {
+        this.#actions.next(new Add(character));
     }
 
-    public async remove(character: CensusCharacter): Promise<void> {
-        const characters = [...this.#characters.value];
-        const index = characters.findIndex((c) => c.id === character.id);
-        if (index > -1) {
-            characters.splice(index, 1);
-            this.#characters.next(characters);
-            await this.saveCharacters(characters);
-        }
+    public remove(character: CensusCharacter): void {
+        this.#actions.next(new Remove(character));
     }
 
     public refreshOnline(): void {
-        this.#refresh.next();
+        this.#actions.next(new Refresh());
     }
 
-    private getCharactersOnline(): OperatorFunction<void, CensusCharacter[]> {
-        return pipe(
-            withLatestFrom(this.characters$),
-            exhaustMap(([_, characters]) => {
-                const url = build({
-                    serviceId: 's:vexedencetracker',
-                    format: 'json',
-                    verb: 'get',
-                    namespace: 'eq2',
-                    collection: 'character',
-                    limit: characters.length,
-                    filter: [
-                        {
-                            field: 'id',
-                            value: characters.map((c) => c.id).join(','),
-                        },
-                    ],
-                    join: [
-                        {
-                            type: 'character_misc',
-                            on: 'id',
-                            to: 'id',
-                            inject_at: 'misc',
-                            show: [
-                                'completed_quest_list',
-                                'quest_list',
-                                'collection_list',
-                            ],
-                        },
-                    ],
-                    tree: [
-                        { start: 'achievements.achievement_list', field: 'id' },
-                        { start: 'misc.quest_list', field: 'crc' },
-                        { start: 'misc.completed_quest_list', field: 'crc' },
-                        { start: 'misc.collection_list', field: 'crc' },
-                    ],
+    public getOnlineCharacters(
+        characters: CensusCharacter[]
+    ): Observable<CensusCharacter[]> {
+        const url = build({
+            serviceId: 's:vexedencetracker',
+            format: 'json',
+            verb: 'get',
+            namespace: 'eq2',
+            collection: 'character',
+            limit: characters.length,
+            filter: [
+                {
+                    field: 'id',
+                    value: characters.map((c) => c.id).join(','),
+                },
+            ],
+            join: [
+                {
+                    type: 'character_misc',
+                    on: 'id',
+                    to: 'id',
+                    inject_at: 'misc',
                     show: [
-                        'displayname',
-                        'name',
-                        'guild',
-                        'achievements.achievement_list',
+                        'completed_quest_list',
+                        'quest_list',
+                        'collection_list',
                     ],
-                });
-                return this.http.get<CharacterSearchResults>(url.href);
-            }),
-            map((data) => {
-                const refreshed = data.character_list;
-                refreshed.sort((a, b) =>
-                    a.displayname.localeCompare(b.displayname)
-                );
-                return refreshed;
-            })
-        );
+                },
+            ],
+            tree: [
+                { start: 'achievements.achievement_list', field: 'id' },
+                { start: 'misc.quest_list', field: 'crc' },
+                { start: 'misc.completed_quest_list', field: 'crc' },
+                { start: 'misc.collection_list', field: 'crc' },
+            ],
+            show: [
+                'displayname',
+                'name',
+                'guild',
+                'achievements.achievement_list',
+            ],
+        });
+        return this.http
+            .get<CharacterSearchResults>(url.href)
+            .pipe(map((data) => data.character_list));
     }
 }
