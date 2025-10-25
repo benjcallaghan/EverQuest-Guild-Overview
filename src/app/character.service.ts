18af28c9dbec0b609d7412671dcb42849e19d9f3
Ben Callaghan <benj.callaghan@gmail.com>
2025-10-25 17:01:54 -0600

    converted online operation to use reactive characters instead of imperative value

--- src/app/character.service.ts
+++ src/app/character.service.ts
@@ -5,10 +5,12 @@ import {
     BehaviorSubject,
     exhaustMap,
     map,
-    Observable,
+    OperatorFunction,
+    pipe,
     Subject,
     Subscription,
     tap,
+    withLatestFrom,
 } from 'rxjs';
 import { CensusCharacter } from './census.service';
 import { build } from './daybreak-census-options';
@@ -39,7 +41,7 @@ export class CharacterService implements OnDestroy {
         this.#refreshSubscription = this.#refresh
             .pipe(
                 tap(() => this.#refreshing.next(true)),
-                exhaustMap(() => this.getCharactersOnline()),
+                this.getCharactersOnline(),
                 tap(() => this.#refreshing.next(false))
             )
             .subscribe(async (refreshed) => {
@@ -81,45 +83,51 @@ export class CharacterService implements OnDestroy {
         this.#refresh.next();
     }
 
-    private getCharactersOnline(): Observable<CensusCharacter[]> {
-        const characters = this.#characters.value;
-        const url = build({
-            serviceId: 's:vexedencetracker',
-            format: 'json',
-            verb: 'get',
-            namespace: 'eq2',
-            collection: 'character',
-            limit: characters.length,
-            filter: [
-                { field: 'id', value: characters.map((c) => c.id).join(',') },
-            ],
-            join: [
-                {
-                    type: 'character_misc',
-                    on: 'id',
-                    to: 'id',
-                    inject_at: 'misc',
-                    show: [
-                        'completed_quest_list',
-                        'quest_list',
-                        'collection_list',
+    private getCharactersOnline(): OperatorFunction<void, CensusCharacter[]> {
+        return pipe(
+            withLatestFrom(this.characters$),
+            exhaustMap(([_, characters]) => {
+                const url = build({
+                    serviceId: 's:vexedencetracker',
+                    format: 'json',
+                    verb: 'get',
+                    namespace: 'eq2',
+                    collection: 'character',
+                    limit: characters.length,
+                    filter: [
+                        {
+                            field: 'id',
+                            value: characters.map((c) => c.id).join(','),
+                        },
                     ],
-                },
-            ],
-            tree: [
-                { start: 'achievements.achievement_list', field: 'id' },
-                { start: 'misc.quest_list', field: 'crc' },
-                { start: 'misc.completed_quest_list', field: 'crc' },
-                { start: 'misc.collection_list', field: 'crc' },
-            ],
-            show: [
-                'displayname',
-                'name',
-                'guild',
-                'achievements.achievement_list',
-            ],
-        });
-        return this.http.get<CharacterSearchResults>(url.href).pipe(
+                    join: [
+                        {
+                            type: 'character_misc',
+                            on: 'id',
+                            to: 'id',
+                            inject_at: 'misc',
+                            show: [
+                                'completed_quest_list',
+                                'quest_list',
+                                'collection_list',
+                            ],
+                        },
+                    ],
+                    tree: [
+                        { start: 'achievements.achievement_list', field: 'id' },
+                        { start: 'misc.quest_list', field: 'crc' },
+                        { start: 'misc.completed_quest_list', field: 'crc' },
+                        { start: 'misc.collection_list', field: 'crc' },
+                    ],
+                    show: [
+                        'displayname',
+                        'name',
+                        'guild',
+                        'achievements.achievement_list',
+                    ],
+                });
+                return this.http.get<CharacterSearchResults>(url.href);
+            }),
             map((data) => {
                 const refreshed = data.character_list;
                 refreshed.sort((a, b) =>
